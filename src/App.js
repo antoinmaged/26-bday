5b0d915f935f3478338b8ee613abf3d32d6e0b02
Antoin Abdelmalek <antoin.abdelmalek@gmail.com>
2023-03-01 16:19:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import sound from "./sound.mp3";
+import celebratemp3 from "./celebrate.mp3";
+import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [currentIndex, setCurrentIndex] = React.useState(-1);
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const audio = new Audio(sound);
+  const celebrate = new Audio(celebratemp3)
+  audio.loop = true;
+  function renderSwitch(param) {
+    switch (param) {
+      case 0:
+        setTimeout(() => {
+          setCurrentIndex(1);
+        }, 14000);
+        return <img src="./wave.gif" />;
+      case 1:
+        setTimeout(() => {
+          setCurrentIndex(2);
+        }, 2000);
+        return <img src="./madness.gif" />;
+        case 2:
+          setTimeout(() => {
+            setCurrentIndex(3);
+          }, 5500);
+          return (<div
+          style={{
+            display: "grid",
+            justifyContent: "center",
+            alignItems: "center",
+            justifyItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div
+            style={{
+              height: "222px",
+              display: "flex",
+              width: "100%",
+            }}
+          >
+            <img src="./t1.gif" />
+            <img src="./t2.gif" />
+            <img src="./t3.gif" />
+          </div>
+        </div>)
+        case 3:
+          setTimeout(() => {
+            setCurrentIndex(4);
+          }, 12000);
+        return (
+          <div
+            style={{
+              display: "grid",
+              justifyContent: "center",
+              alignItems: "center",
+              justifyItems: "center",
+            }}
+          >
+            <div
+              style={{
+                height: "222px",
+                display: "flex",
+                width: "100%",
+              }}
+            >
+              <img src="./1pm.gif" />
+              <img src="./friday.gif" />
+            </div>
+            <div
+              style={{
+                height: "222px",
+                display: "flex",
+                width: "100%",
+              }}
+            >
+              <img src="./3pm.gif" />
+              <img src="./Saturday.gif" />
+            </div>
+            <div
+              style={{
+                height: "222px",
+                display: "flex",
+                width: "100%",
+              }}
+            >
+              <img src="./nvm.gif" />
+              <img src="./Sunday.gif" />
+            </div>
+          </div>
+        );
+        case 4:
+            setCurrentIndex(5)
+            celebrate.loop = true;
+            celebrate.play();
+        return <img src="./happybday.gif" />;
+        case 5:
+          return <img src="./happybday.gif" />;
+      default:
+        return <h1 style={{ color: 'wheat'}}> Please check your sound is on ! then tap</h1>;
+    }
+  }
+  return (
+    <div
+      className="App"
+      style={{
+        width: "100%",
+        height: "1080px",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        display: "flex",
+        flexWrap: "nowrap",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundImage: "url('./bgd.gif')",
+      }}
+      onClick={() => {
+        setCurrentIndex(0);
+        audio.loop = false;
+        audio.play();
+      }}
+    >
+      <div>{renderSwitch(currentIndex)}</div>
     </div>
   );
 }
